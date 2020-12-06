36761cd8c87205a609bf5994672d5aa142bf36f5
Fazlul Hoque <sawravm299@gmail.com>
2020-12-06 22:25:27 +0600

    design done

---
+++ app/src/main/java/com/sawrav/daktarlagbedatacollection/ClinicDoctorList.java
@@ -0,0 +1,14 @@
+package com.sawrav.daktarlagbedatacollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClinicDoctorList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clinic_doctor_list);
+    }
+}
